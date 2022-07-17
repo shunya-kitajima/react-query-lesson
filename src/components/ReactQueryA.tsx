122771a582aa31acefaeb16c6d078b4df6ca1f1e
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-07-17 20:54:51 +0900

    display data

--- src/components/ReactQueryA.tsx
+++ src/components/ReactQueryA.tsx
@@ -8,9 +8,21 @@ const ReactQueryA: VFC = () => {
   const navigate = useNavigate()
   const { status, data } = useQueryTasks()
 
-  if (isLoading) return <div>{'Loading...'}</div>
-  if (isError) return <div>{'Error'}</div>
-  return <div>ReactQueryA</div>
+  if (status === 'loading') return <div>{'Loading...'}</div>
+  if (status === 'error') return <div>{'Error'}</div>
+  return (
+    <>
+      <p className="font-bold my-3">ReactQueryA</p>
+      {data?.map((task) => (
+        <p key={task.id}>{task.title}</p>
+      ))}
+      <ChevronDoubleRightIcon
+        onClick={() => navigate('/query-b')}
+        className="h-5 w-5 mt-2 text-blue-500 cursor-pointer"
+      />
+      <p className="text-sm">fetch B</p>
+    </>
+  )
 }
 
 export default ReactQueryA
