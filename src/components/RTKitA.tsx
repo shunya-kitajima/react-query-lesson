d546dc934059d9b4ee900e10ba9243c58c2eeffa
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-07-20 15:33:01 +0900

    modify

--- src/components/RTKitA.tsx
+++ src/components/RTKitA.tsx
@@ -8,7 +8,18 @@ const RTKitA: VFC = () => {
   const dispatch = useAppDispatch()
   console.log('rendered RTKitA')
 
-  return <div>RTKitA</div>
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <p className="font-bold my-3">RTKitA</p>
+      {count}
+      <button
+        className="py-2 px-3 text-sm text-white bg-indigo-600 hover:bg-indigo-700 rounded"
+        onClick={() => dispatch(increment())}
+      >
+        increment
+      </button>
+    </div>
+  )
 }
 
 export default RTKitA
