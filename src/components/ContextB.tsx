f5038525810ae09df02226eed151e78dc261bae8
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-07-20 14:58:20 +0900

    modify css

--- src/components/ContextB.tsx
+++ src/components/ContextB.tsx
@@ -10,7 +10,7 @@ const ContextB: VFC = () => {
     <div className="flex justify-center items-center flex-col">
       <p className="font-bold my-3">ContextB</p>
       <button
-        className="py-2 px-3 text-sm text-white bg-indigo-600 hover:bg-indigo-700 rounded"
+        className="py-2 px-3 text-sm text-white bg-green-600 hover:bg-green-700 rounded"
         onClick={() => setDark(false)}
       >
         dark mode off
