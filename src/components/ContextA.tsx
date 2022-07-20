2452ed7dd3c9943cd90101026c5bdd3045082ceb
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-07-20 14:42:53 +0900

    create component

--- src/components/ContextA.tsx
+++ src/components/ContextA.tsx
@@ -2,7 +2,21 @@ import { VFC } from 'react'
 import { useStateContext } from '../context/StateProvider'
 
 const ContextA: VFC = () => {
-  return <div>ContextA</div>
+  const { dark, setDark } = useStateContext()
+  console.log('rendered ContextA')
+
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <p className="font-bold my-3">ContextA</p>
+      <p className="my-3">{dark ? 'on' : 'off'}</p>
+      <button
+        className="py-2 px-3 text-sm text-white bg-indigo-600 hover:bg-indigo-700 rounded"
+        onClick={() => setDark(!dark)}
+      >
+        toggle mode
+      </button>
+    </div>
+  )
 }
 
 export default ContextA
